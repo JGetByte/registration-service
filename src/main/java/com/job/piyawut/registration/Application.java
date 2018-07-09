acc1f917503ade02298660f8deede10121bd2ff5
job.piyawut <jan_tao21@hotmail.com>
2018-07-09 20:44:05 +0700

    update configuration

--- src/main/java/com/job/piyawut/registration/Application.java
+++ src/main/java/com/job/piyawut/registration/Application.java
@@ -2,13 +2,14 @@ package com.job.piyawut.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  *
  * @author Piyawut Chiradejnunt<pchiradejnunt@gmail.com>
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
